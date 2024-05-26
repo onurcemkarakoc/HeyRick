b2f6f1286c9070a8aca9c3af01ed5895b4d9c527
onurcemkarakoc <onurcemkobala@gmail.com>
2024-05-26 15:55:59 +0300

    Add basically network module and KtorClient after call api from app module etc.

--- app/src/main/java/com/onurcemkarakoc/heyrick/MainActivity.kt
+++ app/src/main/java/com/onurcemkarakoc/heyrick/MainActivity.kt
@@ -3,26 +3,48 @@ package com.onurcemkarakoc.heyrick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.onurcemkarakoc.heyrick.ui.theme.HeyRickTheme
+import com.onurcemkarakoc.network.Character
+import com.onurcemkarakoc.network.KtorClient
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
+
+    private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var character by remember { mutableStateOf<Character?>(null) }
+
+            LaunchedEffect(key1 = Unit) {
+                delay(2000)
+                character = ktorClient.getCharacter(1)
+            }
             HeyRickTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Greeting("Android")
+                        Text(text = character?.name ?: "Empty Character")
+
+                    }
                 }
             }
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "HeyRick"
 include(":app")
+include(":network")
