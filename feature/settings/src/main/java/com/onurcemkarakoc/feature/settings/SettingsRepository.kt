706137fc08b43e0348d18d2a86982bdf624e8f9d
onurcemkarakoc <onurcemkobala@gmail.com>
2024-11-24 17:02:58 +0300

    Clear flick for getting default app settings.

--- feature/settings/src/main/java/com/onurcemkarakoc/feature/settings/SettingsRepository.kt
+++ feature/settings/src/main/java/com/onurcemkarakoc/feature/settings/SettingsRepository.kt
@@ -6,29 +6,32 @@ import com.onurcemkarakoc.core.data.Language
 import com.onurcemkarakoc.core.data.Theme
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 
 class SettingsRepository @Inject constructor(private val dataStoreAppSettings: DataStore<AppSettings>) {
 
-    suspend fun getTheme() = getAppSettings().theme
+    fun getTheme() = getAppSettings().theme
 
     suspend fun setTheme(theme: Theme) {
         val appSettings = getAppSettings()
         saveAppSettings(appSettings.copy(theme = theme))
     }
 
-    suspend fun getLanguage() = getAppSettings().language
+    fun getLanguage() = getAppSettings().language
 
     suspend fun setLanguage(language: Language) {
         val appSettings = getAppSettings()
         saveAppSettings(appSettings.copy(language = language))
     }
 
-    private suspend fun getAppSettings() = dataStoreAppSettings.data.catch {
-        it.printStackTrace()
-        emit(AppSettings())
-    }.first()
+    private fun getAppSettings() = runBlocking {
+        dataStoreAppSettings.data.catch {
+            it.printStackTrace()
+            emit(AppSettings())
+        }.first()
+    }
 
     private suspend fun saveAppSettings(appSettings: AppSettings) {
         dataStoreAppSettings.updateData {
